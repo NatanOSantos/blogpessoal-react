d12688cd5654ccb2e1070468020ee81e49038d32
Natan Oliveira Santos <natanoliveirasantosmed@gmail.com>
2024-02-06 17:59:02 -0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-
-import * as ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import * as React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
